0c0c4392f1702cc2ee3be73bcd70ac5c63a7451d
Tristan <127442375+tyoung-99@users.noreply.github.com>
2023-12-11 16:11:05 -0500

    Day 4, Part 2

--- src/day_4/day_4.py
+++ src/day_4/day_4.py
@@ -26,4 +26,26 @@ def part_1():
     # print(card_points)
     print(f"Sum of point values: {sum(card_points)}")
 
-part_1()
+def part_2():
+    input_file = open("src/day_4/puzzle_input.txt", "r", encoding="utf-8")
+    cards = input_file.readlines()
+    card_counts = [1] * len(cards)
+
+    for i, card in enumerate(cards):
+        _, card = re.split(": +", card)
+        winning_nums, our_nums = re.split(r" \| +", card)
+        winning_nums = winning_nums.split()
+        our_nums = our_nums.split()
+
+        matches = 0
+        for check in our_nums:
+            if check in winning_nums:
+                matches += 1
+
+        for j in range(1, matches + 1):
+            card_counts[i + j] += card_counts[i]
+
+    # print(card_counts)
+    print(f"Total number of cards: {sum(card_counts)}")
+
+part_2()
