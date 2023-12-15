dde4b10decbdbd8714ae667201421c93de3420f6
Tristan <127442375+tyoung-99@users.noreply.github.com>
2023-12-15 18:01:57 -0500

    Day 9, Part 2

--- src/day_9/day_9.py
+++ src/day_9/day_9.py
@@ -18,18 +18,47 @@ def part_1():
                 differences.append(reading - reading_set[0][i - 1])
             reading_set.insert(0, differences)
 
-    sum = 0
+    sum_vals = 0
     for reading_set in readings:
         for i, reading in enumerate(reading_set):
             # Don't actually need to update 1st set, since new val will be 0 like current last val
             if i == 0:
                 continue
             reading.append(reading[-1] + reading_set[i - 1][-1])
-        sum += reading[-1]
+        sum_vals += reading[-1]
 
-    print(f"Sum of next values: {sum}")
+    print(f"Sum of next values: {sum_vals}")
+
+
+def part_2():
+    readings = []
+    for line in puzzle:
+        reading_set = []
+        reading_set.append([int(x) for x in line.split()])
+        readings.append(reading_set)
+
+    for reading_set in readings:
+        differences = reading_set[0]
+        while any(item != 0 for item in differences):
+            differences = []
+            for i, reading in enumerate(reading_set[0]):
+                if i == 0:
+                    continue
+                differences.append(reading - reading_set[0][i - 1])
+            reading_set.insert(0, differences)
+
+    sum_vals = 0
+    for reading_set in readings:
+        for i, reading in enumerate(reading_set):
+            # Don't actually need to update 1st set, since new val will be 0 like current last val
+            if i == 0:
+                continue
+            reading.insert(0, reading[0] - reading_set[i - 1][0])
+        sum_vals += reading[0]
+
+    print(f"Sum of next values: {sum_vals}")
 
 
 with open("src/day_9/puzzle_input.txt", "r", encoding="utf-8") as f:
     puzzle = f.readlines()
-part_1()
+part_2()
