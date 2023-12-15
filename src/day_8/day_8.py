0366ce94ffb26c1258d6cc8171e7de67c8ded908
Tristan <127442375+tyoung-99@users.noreply.github.com>
2023-12-15 17:11:18 -0500

    Day 8, Part 2

--- src/day_8/day_8.py
+++ src/day_8/day_8.py
@@ -1,6 +1,7 @@
 # Day 8
 
 import re
+from math import lcm
 
 
 def part_1():
@@ -29,4 +30,41 @@ def part_1():
     print(f"Steps required: {step_count}")
 
 
-part_1()
+def part_2():
+    input_file = open("src/day_8/puzzle_input.txt", "r", encoding="utf-8")
+    steps = input_file.readlines()
+    nodes = steps[2:]
+    steps = steps[0].strip()
+
+    new_nodes = {}
+    for line in nodes:
+        line = re.sub(r" = \(|, |\)", " ", line)
+        line = line.split()
+        new_nodes[line[0]] = {"L": line[1], "R": line[2]}
+    nodes = new_nodes
+
+    starts = []
+    for node in nodes:
+        if node[2] == "A":
+            starts.append(node)
+
+    step_counts = []
+    for current in starts:
+        step_count = 0
+        step_index = 0
+        while current[2] != "Z":
+            current = nodes[current][steps[step_index]]
+            step_count += 1
+            step_index += 1
+            if step_index >= len(steps):
+                step_index = 0
+        step_counts.append(step_count)
+
+    lowest = 1
+    for steps in step_counts:
+        lowest = lcm(lowest, steps)
+
+    print(f"Steps required: {lowest}")
+
+
+part_2()
