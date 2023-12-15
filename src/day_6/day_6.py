7981d14f0ef4a3490c82efd4a04668f7812297f3
Tristan <127442375+tyoung-99@users.noreply.github.com>
2023-12-15 14:28:50 -0500

    Day 6, Part 2

--- src/day_6/day_6.py
+++ src/day_6/day_6.py
@@ -25,7 +25,27 @@ def part_1():
         winners.append(time - held * 2 + 1)
 
     # print(winners)
-    print(f"Lowest location: {np.prod(winners)}")
+    print(f"Product of winning methods: {np.prod(winners)}")
 
 
-part_1()
+def part_2():
+    input_file = open("src/day_6/puzzle_input.txt", "r", encoding="utf-8")
+    time, record = input_file.readlines()
+    time = re.sub(r"\s+", "", time)
+    _, time = re.split(":", time)
+    time = int(time)
+    record = re.sub(r"\s+", "", record)
+    _, record = re.split(":", record)
+    record = int(record)
+
+    held = 1  # In ms
+    # Distance = time held * (total time - time held)
+    while held * (time - held) <= record:
+        held += 1
+    # Num possibilties = max time - time held - (time held - 1), b/c graph is bell curve
+    winners = time - held * 2 + 1
+
+    print(f"Number of winning methods: {winners}")
+
+
+part_2()
