76191eb7c4bd36753b53e7252706c8eaad7fe906
Tristan <127442375+tyoung-99@users.noreply.github.com>
2023-12-20 15:38:34 -0500

    Day 12, Part 2

--- src/day_12/day_12.py
+++ src/day_12/day_12.py
@@ -35,7 +35,53 @@ def part_1(puzzle):
     print(f"Sum of counts: {sum(combos)}")
 
 
+def part_2(puzzle):
+    def handle_broken(springs, groups):
+        next_group = groups[0]
+        cut = springs[:next_group]
+        delimiter = springs[next_group] if next_group < len(springs) else None
+        if delimiter == "#" or "." in cut:
+            return 0
+        else:
+            return find_combos(springs[next_group + 1 :], groups[1:])
+
+    def handle_working(springs, groups):
+        return find_combos(springs[1:], groups)
+
+    def find_combos(springs, groups):
+        key = (springs, tuple(groups))
+        if key in combos_seen:
+            return combos_seen[key]
+        if sum(groups) + len(groups) - 1 > len(springs):
+            result = 0
+        elif groups == []:
+            if any(spring == "#" for spring in springs):
+                result = 0
+            else:
+                result = 1
+        elif springs[0] == "#":
+            result = handle_broken(springs, groups)
+        elif springs[0] == ".":
+            result = handle_working(springs, groups)
+        elif springs[0] == "?":
+            result = handle_broken(springs, groups) + handle_working(springs, groups)
+
+        combos_seen[key] = result
+        return result
+
+    combos = []
+    combos_seen = {}
+    for line in puzzle:
+        springs, groups = line.strip().split()
+        groups = [int(x) for x in groups.split(",")]
+        springs = "?".join([springs] * 5)
+        groups *= 5
+        combos.append(find_combos(springs, groups))
+
+    # print(combos)
+    print(f"Sum of counts: {sum(combos)}")
+
+
 with open("src/day_12/puzzle_input.txt", "r", encoding="utf-8") as f:
     puzzle = f.readlines()
-part_1(puzzle)
-print()
+part_2(puzzle)
